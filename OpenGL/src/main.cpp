65ee853c0bd234bd20ce540e44a1485ed91bfd09
Lambert-pl <68654224+Lambert-pl@users.noreply.github.com>
2022-04-05 16:51:50 +0800

    获取OpenGL错误信息并中断

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -7,6 +7,28 @@
 #include <glad/glad.h>
 
 
+#define ASSERT(x) if(!(x)) __debugbreak();
+#define GLCALL(x) GLClearError();\
+	x;\
+	ASSERT(GLLogCall(#x, __FILE__, __LINE__));
+
+static void GLClearError()
+{
+	while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+	while (unsigned int error = glGetError())
+	{
+		std::cout << "[OpenGL Error] " << error << " Function: " << function 
+			<< " File: " << file << " Line: " << line << std::endl;
+		return false;
+	}
+	return true;
+}
+
+
 struct ShaderSource
 {
 	std::string vertexShaderSource;
@@ -50,19 +72,46 @@ static ShaderSource PaserShaderFile(const std::string& filepath)
 //编译shader
 static unsigned int CompileShader(const char* shaderSource, unsigned int shaderType)
 {
-	unsigned int shader = glCreateShader(shaderType);
-	glShaderSource(shader, 1, &shaderSource, NULL);
-	glCompileShader(shader);
+	GLCALL(unsigned int shader = glCreateShader(shaderType));
+	GLCALL(glShaderSource(shader, 1, &shaderSource, NULL));
+	GLCALL(glCompileShader(shader));
 	int success;
-	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	GLCALL(glGetShaderiv(shader, GL_COMPILE_STATUS, &success));
 	if (!success)
 	{
 		char message[255];
-		glGetShaderInfoLog(shader, 255, NULL, message);
+		GLCALL(glGetShaderInfoLog(shader, 255, NULL, message));
 		std::cout << "Error: Failed To Compile Shader!" << message << std::endl;
 		return 0;
 	}
 	return shader;
+}
+
+//创建shader
+unsigned int CreateShader(const char* vertexSource, const char* fragmentSource)
+{
+	unsigned int vertexShader = CompileShader(vertexSource, GL_VERTEX_SHADER);
+	unsigned int fragmentShader = CompileShader(fragmentSource, GL_FRAGMENT_SHADER);
+
+	// 创建program
+	GLCALL(unsigned int program = glCreateProgram());
+	GLCALL(glAttachShader(program, vertexShader));
+	GLCALL(glAttachShader(program, fragmentShader));
+	GLCALL(glLinkProgram(program));
+
+	int success;
+	GLCALL(glGetProgramiv(program, GL_LINK_STATUS, &success));
+	if (!success)
+	{
+		char message[255];
+		GLCALL(glGetProgramInfoLog(program, 255, NULL, message));
+		std::cout << "Error: Failed To Compile Shader!" << message << std::endl;
+		return 0;
+	}
+	GLCALL(glDeleteShader(vertexShader));
+	GLCALL(glDeleteShader(fragmentShader));
+
+	return program;
 }
 
 int main()
@@ -90,50 +139,34 @@ int main()
 		 0.0f,  0.5f, 0.0f
 	};
 	unsigned int vertextBuffer;
-	glGenBuffers(1, &vertextBuffer);//1:生成的buffer数量
-	glBindBuffer(GL_ARRAY_BUFFER, vertextBuffer);
-	glBufferData(GL_ARRAY_BUFFER, 3 * 3 * sizeof(float), vertices, GL_STATIC_DRAW);
+	GLCALL(glGenBuffers(1, &vertextBuffer));//1:生成的buffer数量
+	GLCALL(glBindBuffer(GL_ARRAY_BUFFER, vertextBuffer));
+	GLCALL(glBufferData(GL_ARRAY_BUFFER, 3 * 3 * sizeof(float), vertices, GL_STATIC_DRAW));
 	//顶点布局
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	GLCALL(glEnableVertexAttribArray(0));
+	GLCALL(glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0));
 
 	//索引缓冲区
 	unsigned int indices[] = {
 		0, 1, 2
 	};
 	unsigned int indexBuffer;
-	glGenBuffers(1, &indexBuffer);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+	GLCALL(glGenBuffers(1, &indexBuffer));
+	GLCALL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer));
+	GLCALL(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
 
 	// 解析shader文件
 	ShaderSource shaderSource = PaserShaderFile("triangle_shader.shader");
 	// 编译shder
-	unsigned int vertexShader = CompileShader(shaderSource.vertexShaderSource.c_str(), GL_VERTEX_SHADER);
-	unsigned int fragmentShader = CompileShader(shaderSource.fragmentShaderSource.c_str(), GL_FRAGMENT_SHADER);
-	// 创建program
-	unsigned int program = glCreateProgram();
-	glAttachShader(program, vertexShader);
-	glAttachShader(program, fragmentShader);
-	glLinkProgram(program);
+	unsigned int program = CreateShader(shaderSource.vertexShaderSource.c_str(), shaderSource.fragmentShaderSource.c_str());
 
-	int success;
-	glGetProgramiv(program, GL_LINK_STATUS, &success);
-	if (!success)
-	{
-		std::cout << "Link Program Failed!" << std::endl;
-		return -1;
-	}
-
-	glUseProgram(program);
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+	GLCALL(glUseProgram(program));
 
 	while (!glfwWindowShouldClose(window))
 	{
-		glClear(GL_COLOR_BUFFER_BIT);
-		glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
-		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+		GLCALL(glClear(GL_COLOR_BUFFER_BIT));
+		GLCALL(glClearColor(0.1f, 0.1f, 0.2f, 1.0f));
+		GLCALL(glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0));
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
